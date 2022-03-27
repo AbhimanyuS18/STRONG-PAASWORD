4c527458b4387ff2a5917d878901db6ed8544288
Abhimanyu Singh <93311022+AbhimanyuS18@users.noreply.github.com>
2022-03-27 16:49:18 +0530

    Add files via upload
    Choose nothing shorter than 15 characters, more if possible. Use a mix of characters. The more you mix up letters (upper-case and lower-case), numbers, and symbols, the more potent your password is, and the harder it is for a brute force attack to crack it. Avoid common substitutions.

---
+++ strong_paasword.py
@@ -0,0 +1,46 @@
+#################  STRONG  PASSWORD #########
+
+
+#pas=input("enter password")
+#if len(pas)>8 and len(pas)<16:
+#    if ("A"in pas or"B" in pas or"C" in pas or"D" in pas or"E"in pas or"F" in pas or"H" in pas or "I"in pas  or "J"in pas or "K"in pas or "L"in pas or "M"in pas or "N"in pas or "O"in pas or "P"in pas or "Q"in pas or "R"in pas or "S"in pas or "T"in pas or "U"in pas or "V"in pas or "W"in pas or "X"in pas or "Y"in pas  or "Z"in pas):
+#        if ("a"in pas or"b"in pas or"c"in pas or"d"in pas or"e"in pas or"f"in pas or "g"in pas or"h"in pas or"i"in pas or"j"in pas or"k"in pas or"l"in pas or"m"in pas or"n"in pas or"o"in pas or"p"in pas or"q"in pas or"r"in pas or"s"in pas or"t"in pas or"u"in pas or"v"in pas or"w"in pas in pas or"x"in pas or"y"in pas or"z"in pas):
+#            if "@"in pas  or "#"in pas or "$"in pas  or "!"in pas:
+#                if ("1"in pas  or "2"in pas  or "3"in pas  or "4"in pas  or "5"in pas  or "6"in pas  or "7"in pas  or "8"in pas or"9"in pas or"0"in pas):
+#                    print("correct password")
+#                else:
+#                    print("enter number")
+#            else:
+#                print("not special character")
+#        else:
+#            print("small")           
+#    else:
+#        print("capital")
+#else:
+#    print("enter 8 to 16 character")
+
+
+######STRONG PASSWORD IMPORT RANDOM#############
+
+
+
+# import random
+# a1 = input("enter the password") 
+# password   = ("a" or "b" or  "c" or "d" or "e" or "f" or "g" or "h" or "i" or "j" or "k" or "l" or "m" or "n" or "o" or "p" or "q" or "r" or "s" or "t"or "u" or "v" or "w" or "x" or "y" or "z") and ("A" or "B" or "C" or "D" or "E" or "F" or "G" or "H" or "I" or "J" or "K" or "L" or "M" or "N" or "O" or "P" or "Q" or "R" or "S" or "T"or "U" or "V" or "W" or "X" or "Y" or"Z") and ("1" or "2" or "3" or "4" or "5" or  "6" or "7" or "8" or "9" or "0") and ("~" or "!" or "@" or "#" or "$" or "%" or  "^" or "&" or  "*" or  "(" or ")" or "o_" or "-" or "=" or "+" or "`" or "," or "." or"/" or"?" or "|" or  "\"" or ":" or ";" or "'")
+# if a1 in password :
+#     print("strong password")
+# else:
+#     print("weak password")
+
+
+# password = input("enter your password")
+# pas = "a" or "b" or "c" or "d" or "e" or "f" or "g" or "h" or "i" or "j" or "k" or "l" or "m" or "n" or "o" or "p" or "q" or "r" or "s" or "t" or "u" or "v" or "w" or "x" or "y" or "z" 
+# pas1 = "A" or "B" or "C" or "D" or "E" or "F" or "G" or "H" or "I" or "J" or "K" or "L" or "M" or "N" or "O" or "P" or "Q" or "R" or "S" or "T" or "U" or "V" or "W" or "X" or "Y" or "Z"
+# pas2 = "1" or "2" or "3" or "4" or "5" or "6" or "7" or "8" or "9" or "0"
+# pas3 = "~" or "`" or "!" or "@" or"#" or "$" or"%" or"^" or"&" or"*" or "(" or ")" or "_" or "-" or "+" or "=" or ":" or ";" or "<" or "'" or ">" or "/" or "?" or "." or "," or " "
+# if password ==  pas :
+#     print()
+
+
+
+# strong password ?
